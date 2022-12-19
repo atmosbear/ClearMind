b691733df709eedc2e4941f2d5ca853885a3452d
atmosbear <mondegreen84@protonmail.com>
2022-12-19 08:35:29 -0800

    Added basic centering forces

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ class Dot {
         public longText: string = "",
         public color: string = "black",
         public radius: number = 10,
+        public linked: Dot[] = []
     ) { }
 }
 
@@ -24,21 +25,38 @@ maximizeCanvas()
 // app setup
 let dots: Dot[] = []
 let possibles = ["a", "b", "c", "q", "peter jackson", "dora"]
-createTestDots(1000)
+createTestDots(10)
 function createTestDots(howMany: number) {
     for (let i = 0; i < howMany; i++) {
-        let x = innerWidth * Math.random()
-        let y = innerHeight * Math.random()
+        let x = innerWidth/2 + 300 * Math.random() - 150
+        let y = innerHeight/2 + 300 * Math.random() - 150
         dots.push(new Dot(possibles[Math.floor(Math.random() * possibles.length)], x, y))
     }
 }
 
-function mutatePos(A: Dot, B: Dot) {
-    let dx = Math.sqrt(Math.abs(A.x - B.x))
-    A.x += dx
-    B.x += -dx
+function mutatePos(A: Dot, B: Dot, dt) {
+    let dx = A.x - B.x
+    let signX = Math.sign(dx)
+    let dy = A.y - B.y
+    let signY = Math.sign(dy)
+    let c1 = 1
+    let c2 = 1
+    let d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+    
+    //central
+    dx = A.x - innerWidth / 2 
+    dy = A.y - innerHeight / 2 
+    signX = Math.sign(dx)
+    signY = Math.sign(dy)
+    let centralForceX = Math.abs(dx) * signX
+    let centralForceY = Math.abs(dy) * signY
+    A.x -= centralForceX * 0.001
+    A.y -= centralForceY * 0.001
+    // B.x -= centralForceX
+    // B.y -= centralForceY
 }
 
+let dt = 0
 function animate(tick) {
     context.clearRect(0, 0, canvas.width, canvas.height)
     for (let i = 0; i < dots.length; i++) {
@@ -48,7 +66,7 @@ function animate(tick) {
             let dot2 = dots[j]
             if (dot !== dot2) {
                 // calculate physics
-                mutatePos(dot, dot2)
+                mutatePos(dot, dot2, dt)
             }
         }
     }
@@ -57,10 +75,10 @@ function animate(tick) {
 
 
         // draw
-        if (dot.x > innerWidth) {
+        if (dot.x > innerWidth || dot.x < 0) {
             dot.x = innerWidth * Math.random()
         }
-        if (dot.y > innerHeight) {
+        if (dot.y > innerHeight || dot.y < 0) {
             dot.y = innerHeight * Math.random()
         }
         context.strokeStyle = "black"
@@ -70,5 +88,6 @@ function animate(tick) {
         context.strokeText(dot.title, dot.x, dot.y)
     }
     requestAnimationFrame(animate)
+    dt = tick - dt
 }
 requestAnimationFrame(animate)
